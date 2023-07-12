d301a0c8b5cb80f479a34de8d3bd7c6a3bbc5e18
Johann Williams <johann.williams01@gmail.com>
2023-07-12 11:03:44 -0500

    updated seeder to wipe table clean before seeding, completed deletion exercise. verified deletions.

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,4 +1,5 @@
 USE codeup_test_db;
+    truncate albums;
 
     insert into albums(artist, name, release_date, sales, genre)
     values  ('Michael Jackson','Thriller', 1982, 47.3,'Pop, Rock, R&B'),
