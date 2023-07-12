92bbc89b6828623a66124db62fe1c1a9d4e17428
Johann Williams <johann.williams01@gmail.com>
2023-07-12 14:02:24 -0500

    updated dates before 1980 to be before 1800's by sub 100 from date.

--- update_exercises.sql
+++ update_exercises.sql
@@ -5,7 +5,7 @@ update albums set sales = sales * 10;
 select * from albums;
 
 select * from albums where release_date < 1980;
-update albums set release_date = 1899 where release_date < 1980;
+update albums set release_date = release_date - 100 where release_date < 1980;
 select * from albums where release_date < 1980;
 
 select * from albums where artist = 'Michael Jackson';
